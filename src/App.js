ed43d28919aa92dfdbb9807d14f2c033f7a9f1ee
Nick Chaloult <npchaloult@gmail.com>
2019-07-19 20:34:32 -0400

    First draft of the two-column full-screen layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <p>
-        Edit src/App.js and save to reload.
-      </p>
+    <div id="container">
+      <div className="outer-column" style={{'background': "#cccccc"}}>
+        <div className="inner-column">
+          <p>the quick brown fox jumps over the lazy dog</p>
+        </div>
+      </div>
+      <div className="outer-column" style={{'background': "#eeeeee"}}>
+        <div className="inner-column">
+          <p>Testing right</p>
+        </div>
+      </div>
     </div>
   );
 }
