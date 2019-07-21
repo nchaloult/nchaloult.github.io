c579101567be1825ccecea0c9a5917f02abc3f5b
Nick Chaloult <npchaloult@gmail.com>
2019-07-20 23:54:23 -0400

    Clicking anywhere on the left half of the page "focuses" the prompt input text field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import Shell from './components/Shell';
 
 function App() {
+  // References the Shell component's div
+  const node = useRef();
+
+  // Each time a click happens, call checkForShellClick
+  useEffect(() => {
+    document.addEventListener('click', checkForShellClick);
+
+    return () => {
+      document.removeEventListener('click', checkForShellClick);
+    };
+  }, []);
+
+  /*
+   * Called each time that a click happens.
+   *
+   * If the click happens inside of the Shell component's div, then redirect
+   * focus onto the Shell component's input text field.
+   */
+  const checkForShellClick = (e) => {
+    if (node.current.contains(e.target)) {
+      document.getElementById('prompt-input-form').elements[0].focus();
+    }
+  };
+
   return (
     <div id="container">
-      <div className="outer-column">
+      <div ref={ node } className="outer-column">
         <div className="inner-column">
           <Shell />
         </div>
--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -5,23 +5,19 @@ import { constants } from '../utils/constantsAndCommands';
 function Prompt(props) {
   return (
     <div id="prompt">
-      <div>
-        <p>{ constants.prompt }</p>
-      </div>
-      <div id="shell-input">
-        <form onSubmit={ props.submitCommand }>
-          <input
-            autoFocus={ true }
-            autoComplete="off"
-            autoCorrect="off"
-            autoCapitalize="off"
-            spellCheck="false"
-            type="text"
-            value={ props.value }
-            onChange={ (e) => props.updateCurInput(e.target.value) }
-          />
-        </form>
-      </div>
+      <p>{ constants.prompt }</p>
+      <form id="prompt-input-form" onSubmit={ props.submitCommand }>
+        <input
+          autoFocus={ true }
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          type="text"
+          value={ props.value }
+          onChange={ (e) => props.updateCurInput(e.target.value) }
+        />
+      </form>
     </div>
   );
 }
