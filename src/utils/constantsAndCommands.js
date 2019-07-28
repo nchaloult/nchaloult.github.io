7a7b2bc90f2b981a2cacc619a41c37801f6a7736
Nick Chaloult <npchaloult@gmail.com>
2019-07-28 11:23:34 -0400

    Reworded/updated short description about self

--- src/utils/constantsAndCommands.js
+++ src/utils/constantsAndCommands.js
@@ -13,7 +13,7 @@ const constants = {
     'Available commands and their arguments:',
     '======================================='
   ],
-  'shortDesc': 'a Computer Science student at Georgia Tech who is eager to learn, and strives to do work that makes themself proud',
+  'shortDesc': 'a Computer Science student at Georgia Tech who is eager to learn and is proud of their work',
 
   'acceptsNoArgsWarning': ' doesn\'t accept any arguments.',
   'argNotRecognizedWarning': ' isn\'t recognized or supported by: ',
