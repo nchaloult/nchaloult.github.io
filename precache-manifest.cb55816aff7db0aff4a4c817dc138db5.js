a0fddff943e28a74299822c03c7248637427ec86
Nick Chaloult <npchaloult@gmail.com>
2019-07-28 11:47:04 -0400

    Updates

---
+++ precache-manifest.cb55816aff7db0aff4a4c817dc138db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc81aeddf0f732fc920c48ae319e7e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f885a272b51aa1c07c5",
+    "url": "/static/css/main.5267c67d.chunk.css"
+  },
+  {
+    "revision": "93c4319319f992928171",
+    "url": "/static/js/2.244b2b74.chunk.js"
+  },
+  {
+    "revision": "9f885a272b51aa1c07c5",
+    "url": "/static/js/main.8a1027a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "32eea4650c5a2af0caf7c834edf9e841",
+    "url": "/static/media/resume.32eea465.pdf"
+  }
+]);
