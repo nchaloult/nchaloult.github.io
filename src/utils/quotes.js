af42c96bda319b2c910cd0135989349def4bd1cd
Nick Chaloult <npchaloult@gmail.com>
2021-03-03 01:58:00 -0500

    Remove quotes that make me seem basic
    💁‍♀️

--- src/utils/quotes.js
+++ src/utils/quotes.js
@@ -5,10 +5,6 @@ const quotes = [
     'https://lists.freebsd.org/pipermail/freebsd-current/2010-August/019310.html',
   ],
   [
-    '"C is quirky, flawed, and an enormous success."',
-    '- Dennis Ritchie',
-  ],
-  [
     '"What does it say about the state of computer science education that one must make a case for teaching how to think clearly?"',
     '- Leslie Lamport',
     'http://lamport.azurewebsites.net/pubs/pubs.html#teaching-concurrency',
@@ -17,10 +13,6 @@ const quotes = [
     '"While fault tolerance is a highly desired goal, it comes with its own pitfalls: it often hides persistent underlying problems. Fault tolerance is fault masking."',
     '- Baker et al.',
     'https://research.google.com/pubs/pub36971.html',
-  ],
-  [
-    '"Tell me and I forget. Show me and I remember. Involve me and I understand."',
-    '- Chinese proverb',
   ],
   [
     '"Do you want me to take your unstructured monolith and break it down into an unstructured set of microservices?"',
