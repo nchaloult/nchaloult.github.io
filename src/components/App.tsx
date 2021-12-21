9d95a02ca520491fb9c44aa2db069f8919026175
Nick Chaloult <npchaloult@gmail.com>
2021-12-21 01:33:06 -0500

    Add Shell component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Shell from './Shell';
+import styles from './App.module.scss';
 
 function App() {
-  return <p>:)</p>;
+  return (
+    <div id={styles.container}>
+      <Shell />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Shell.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Shell.module.scss';
+
+function Shell() {
+  return <div id={styles.container}>shell component</div>;
+}
+
+export default Shell;
