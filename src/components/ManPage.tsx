969839707d0215b3e5499d931654194d90491065
Nick Chaloult <npchaloult@gmail.com>
2021-12-21 13:55:35 -0500

    Add hard coded options section

--- src/components/ManPage.tsx
+++ src/components/ManPage.tsx
@@ -35,6 +35,48 @@ function ManPage() {
             <b>{command.name}</b> {manPage.desc}
           </div>
         </div>
+        <div>
+          <b>OPTIONS</b>
+          <div className={styles.indented} id={styles.options_content}>
+            {/* TODO: make these dynamic based on the supported commands */}
+            <div>
+              <b>-a, --all</b>
+              <div className={styles.indented}>
+                Prints lots of Nick&apos;s important info
+              </div>
+            </div>
+            <div>
+              <b>-e, --email</b>
+              <div className={styles.indented}>
+                Prints Nick&apos;s email address
+              </div>
+            </div>
+            <div>
+              <b>-g, --github</b>
+              <div className={styles.indented}>
+                Prints a link to Nick&apos;s GitHub profile
+              </div>
+            </div>
+            <div>
+              <b>-i, --interests</b>
+              <div className={styles.indented}>
+                Prints what Nick is currently interested in
+              </div>
+            </div>
+            <div>
+              <b>-l, --linkedin</b>
+              <div className={styles.indented}>
+                Prints a link to Nick&apos;s LinkedIn profile
+              </div>
+            </div>
+            <div>
+              <b>-r, --resume</b>
+              <div className={styles.indented}>
+                Prints a link to view Nick&apos;s resume as a PDF
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={styles.margin_row}>
         <span>{manPage.fullName}</span>
