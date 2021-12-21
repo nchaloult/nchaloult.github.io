b46a318a716c0b2c1206e6872455dac5adc261c8
Nick Chaloult <npchaloult@gmail.com>
2021-12-21 01:35:30 -0500

    Add ManPage component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Shell from './Shell';
 import styles from './App.module.scss';
+import ManPage from './ManPage';
 
 function App() {
   return (
     <div id={styles.container}>
       <Shell />
+      <ManPage />
     </div>
   );
 }
---
+++ src/components/ManPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './ManPage.module.scss';
+
+function ManPage() {
+  return <div id={styles.container}>man page component</div>;
+}
+
+export default ManPage;
