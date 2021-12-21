290c08ac623d23fecf72d39ba437f12c6a67dfa1
Nick Chaloult <npchaloult@gmail.com>
2021-12-21 13:09:18 -0500

    Add margin header on man page

--- src/components/ManPage.tsx
+++ src/components/ManPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import styles from './ManPage.module.scss';
 
+import { command, manPage } from '../resources/strings';
+
 function ManPage() {
-  return <div id={styles.container}>man page component</div>;
+  return (
+    <div id={styles.container}>
+      <div className={styles.margin_row}>
+        <span>
+          {command.name.toUpperCase()}({manPage.sectionNumber})
+        </span>
+        <span>nchaloult.com General Commands Manual</span>
+        <span>
+          {command.name.toUpperCase()}({manPage.sectionNumber})
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default ManPage;
