b184a7bfde15c523a64ce484f61279a813e47e75
Nick Chaloult <npchaloult@gmail.com>
2021-12-21 13:37:18 -0500

    Complete fleshing out description

--- src/resources/strings.ts
+++ src/resources/strings.ts
@@ -1,7 +1,7 @@
 export const command = {
   name: 'nick',
   interests:
-    'Rust, consensus algorithms, distributed systems, and free and open-source software',
+    'Rust, consensus algorithms, distributed systems, and free and open-source projects',
 };
 
 export const manPage = {
@@ -10,5 +10,5 @@ export const manPage = {
   fullName: 'Nicholas Chaloult',
   shortDesc:
     'a Computer Science graduate from Georgia Tech who is eager to learn about new technologies and best practices',
-  desc: `enjoys discovering better ways to write maintainable software. Currently interested in ${command.interests}`,
+  desc: `enjoys discovering better ways to write maintainable software. Currently interested in ${command.interests}. Concentrations in school were Intelligence (AI & ML) and Information and Internetworks (database technologies).`,
 };
