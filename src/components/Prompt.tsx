c18bf38d286118a20f4bdaabd91d050d79c490ea
Nick Chaloult <npchaloult@gmail.com>
2021-12-30 18:27:38 -0500

    Maintain local state of the command to run

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Prompt.module.scss';
 import { prompt } from '../resources/strings';
 
 function Prompt() {
+  const [cmd, setCmd] = useState('');
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    alert(`running the command: ${cmd}`);
+    setCmd('');
+  }
+
   return (
     <div id={styles.container}>
       <b>{prompt}</b>
-      <form id={styles.input}>
+      <form id={styles.input} onSubmit={handleSubmit}>
         <input
           autoFocus
           autoComplete="off"
@@ -14,6 +22,8 @@ function Prompt() {
           autoCapitalize="off"
           spellCheck="false"
           type="text"
+          value={cmd}
+          onChange={(e) => setCmd(e.target.value)}
         />
       </form>
     </div>
