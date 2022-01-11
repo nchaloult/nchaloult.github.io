fb9c2a6111a6bf93fdde22d9cbb6ce6549f18f54
Nick Chaloult <npchaloult@gmail.com>
2022-01-11 17:35:46 -0500

    Add color and styling to the `help` program's output

--- src/commands/help.tsx
+++ src/commands/help.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AcceptsNoArgsError, Program } from '.';
+import styles from './Help.module.scss';
 
 export default class Help implements Program {
   run(options: Set<string>): JSX.Element {
@@ -9,32 +10,39 @@ export default class Help implements Program {
 
     return (
       <>
-        <span>How to Use Commands</span>
-        <span>===================</span>
-        <span>Type &quot;nick -a&quot;, then press Enter.</span>
+        <b>How to Use Commands</b>
+        <b>===================</b>
+        <span>
+          Type <b className={styles.highlighted}>nick -a</b>, then press Enter.
+        </span>
         <br />
         <span>
-          &quot;nick&quot; is the program you&apos;re invoking, and
-          &quot;-a&quot; is an option that sends specific instructions to that
-          program.
+          <b className={styles.highlighted}>nick</b> is the program you&apos;re
+          invoking, and <b className={styles.highlighted}>-a</b> is an option
+          that sends specific instructions to that program.
         </span>
         <br />
         <span>
           You can provide more than one option at a time. Try running:
         </span>
-        <span>&nbsp;&nbsp;- &quot;nick -egl&quot;</span>
-        <span>&nbsp;&nbsp;- &quot;nick --linkedin --resume&quot;</span>
+        <span>
+          &nbsp;&nbsp;- <b className={styles.highlighted}>nick -egl</b>
+        </span>
+        <span>
+          &nbsp;&nbsp;-{' '}
+          <b className={styles.highlighted}>nick --linkedin --resume</b>
+        </span>
         <span>&nbsp;&nbsp;- etc.</span>
         <br />
-        <span>Available Programs</span>
-        <span>==================</span>
+        <b>Available Programs</b>
+        <b>==================</b>
         {/* TODO: Make this content dynamic based on the `programs` map in
         index.tsx */}
-        <span>clear</span>
-        <span>fortune</span>
-        <span>help</span>
-        <span>nick</span>
-        <span>whoami</span>
+        <b className={styles.highlighted}>clear</b>
+        <b className={styles.highlighted}>fortune</b>
+        <b className={styles.highlighted}>help</b>
+        <b className={styles.highlighted}>nick</b>
+        <b className={styles.highlighted}>whoami</b>
       </>
     );
   }
