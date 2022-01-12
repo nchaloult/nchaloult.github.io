f31f9bb1d6ef3030b87f81a2de1a5d8fa036ebf0
Nick Chaloult <npchaloult@gmail.com>
2022-01-12 02:09:45 -0500

    Clean up warnings to do with unused imports and function params

--- src/commands/clear.tsx
+++ src/commands/clear.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { AcceptsNoArgsError, Program } from '.';
 
 // This program only needs to be here so that if somebody tries to invoke it
 // with options, the proper error message is displayed ("clear doesn't accept
 // any arguments" or whatever).
 export default class Clear implements Program {
-  run(options: Set<string>): JSX.Element {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  run(_options: Set<string>): JSX.Element {
     throw new AcceptsNoArgsError();
   }
 }
