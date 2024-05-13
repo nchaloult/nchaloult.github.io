4effc0736fe2cda6618432101847ee84480f7f7a
Nick Chaloult <npchaloult@gmail.com>
2024-05-13 09:57:07 -0400

    Remove "city is a large community" quote

--- src/quotes.ts
+++ src/quotes.ts
@@ -58,10 +58,6 @@ export const quotes: Quote[] = [
     },
   },
   {
-    text: 'A city is a large community where people are lonesome together.',
-    author: 'Herbert V. Prochnow',
-  },
-  {
     text: 'Every kid starts out as a natural-born scientist, and then we beat it out of them. A few trickle through the system with their wonder and enthusiasm for science intact.',
     author: 'Carl Sagan',
   },
