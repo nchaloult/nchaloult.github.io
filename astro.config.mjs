54f76073ac840d8a9d4123a26d3bce1a47810776
Nick Chaloult <npchaloult@gmail.com>
2025-08-27 16:18:27 -0400

    Install astro's react and tailwind integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://nchaloult.com",
-    trailingSlash: "never"
+  site: "https://nchaloult.com",
+  trailingSlash: "never",
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
