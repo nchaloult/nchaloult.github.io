472a9c22e57fe639f8df34de7e531aff9a5e7f31
Nick Chaloult <npchaloult@gmail.com>
2025-09-05 15:07:28 -0400

    Remove lingering, unused files from Astro boilerplate + old site version

--- src/components/PromptPrefix.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './PromptPrefix.module.scss';
-
-function PromptPrefix() {
-  return <b id={styles.prompt}>guest@nchaloult.com:~$</b>;
-}
-
-export default PromptPrefix;
