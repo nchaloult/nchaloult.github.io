2fa78c5bd257f6660600c556c73d49b2e1900027
Nick Chaloult <npchaloult@gmail.com>
2025-10-21 14:09:46 -0400

    Add missing punctuation to a quote

--- src/quotes.ts
+++ src/quotes.ts
@@ -121,7 +121,7 @@ export const quotes: Quote[] = [
     },
   },
   {
-    text: 'Imagine engineering a system to be as reliable as possible and then having your success be taken as evidence that you are useless',
+    text: 'Imagine engineering a system to be as reliable as possible and then having your success be taken as evidence that you are useless.',
     author: 'Vaibhav Sagar',
     source: {
       text: 'https://twitter.com/vbhvsgr/status/1594088007585259520',
