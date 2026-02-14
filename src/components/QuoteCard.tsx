6d29ba0c8780bac0903de053ed93bfe9049f8d2a
Nick Chaloult <npchaloult@gmail.com>
2026-02-13 22:02:12 -0500

    Perform quote shuffling at SSG time to eliminate a content flash

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -1,11 +1,6 @@
-import {
-  useMemo,
-  useState,
-  type PropsWithChildren,
-  type ReactNode,
-} from "react";
+import { useState, type PropsWithChildren, type ReactNode } from "react";
 import RerollIcon from "../icons/RerollIcon.tsx";
-import { quotes, type NumberedQuote } from "../quotes.ts";
+import { type NumberedQuote } from "../quotes.ts";
 import { AnimatePresence, motion } from "motion/react";
 
 interface SlideTransitionProps {
@@ -48,61 +43,19 @@ function SlideTransition(props: PropsWithChildren<SlideTransitionProps>) {
   );
 }
 
-// Knuth shuffle algo implementation.
-// https://stackoverflow.com/a/2450976
-function shuffle<T>(array: T[]): T[] {
-  const result = [...array];
-  let currentIndex = result.length;
-
-  // While there remain elements to shuffle...
-  while (currentIndex !== 0) {
-    // Pick a remaining element...
-    const randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [result[currentIndex], result[randomIndex]] = [
-      result[randomIndex],
-      result[currentIndex],
-    ];
-  }
-
-  return result;
-}
-
-export default function QuoteCard() {
-  // Add an incrementing integer ID to each quote according to their order in
-  // src/quotes.ts. Done here instead of manually inside each member of
-  // src/quotes.ts's quotes array.
-  //
-  // Instead of picking a random quote from the array of quotes each time,
-  // we shuffle the array the first time this component is rendered. The
-  // "reroll" button advances to the next quote in the array.
-  //
-  // This makes the user experience better: you won't see the same quote more
-  // than once until you've seen all of them.
-  const shuffledQuotes = useMemo(() => {
-    const numberedQuotes: NumberedQuote[] = quotes.map((quote, i) => ({
-      num: i + 1,
-      ...quote,
-    }));
-    return shuffle(numberedQuotes);
-  }, []);
-
+export default function QuoteCard({ quotes }: { quotes: NumberedQuote[] }) {
   const [curQuoteIdx, setCurQuoteIdx] = useState(0);
-  const curQuote = shuffledQuotes[curQuoteIdx];
+  const curQuote = quotes[curQuoteIdx];
 
   return (
     <aside className="flex h-48 w-full max-w-2xl flex-col gap-2">
       <header className="flex items-end justify-between border-b border-gruvbox-bg1 pb-2">
         <h4 className="w-fit text-sm text-gruvbox-gray">
-          Quote #{curQuote.num} of {shuffledQuotes.length}
+          Quote #{curQuote.num} of {quotes.length}
         </h4>
         <button
           type="button"
-          onClick={() =>
-            setCurQuoteIdx((prev) => (prev + 1) % shuffledQuotes.length)
-          }
+          onClick={() => setCurQuoteIdx((prev) => (prev + 1) % quotes.length)}
           className="transition:scale relative flex cursor-pointer items-center gap-2 rounded-lg bg-gruvbox-bg0-s px-4 py-2 text-sm shadow-md ring-1 ring-neutral-950/15 duration-75 before:pointer-events-none before:absolute before:inset-0 before:rounded-lg before:shadow-highlight before:shadow-white/5 hover:bg-gruvbox-bg1 active:scale-94"
         >
           <span className="sr-only">Reroll</span>
