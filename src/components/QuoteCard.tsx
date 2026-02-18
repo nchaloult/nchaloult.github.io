561eddcef97ff02567ff845b323cde0caac26522
Nick Chaloult <npchaloult@gmail.com>
2026-02-18 14:49:10 -0500

    Make the QuoteCard's "reroll" button more mobile-friendly

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -3,6 +3,13 @@ import RerollIcon from "../icons/RerollIcon.tsx";
 import { type NumberedQuote } from "../quotes.ts";
 import { AnimatePresence, motion } from "motion/react";
 
+const isTouchDevice =
+  // Set this to false during Astro's SSG pass. Only attempt to perform this
+  // check once this QuoteCard component is hydrating on the client.
+  typeof window !== "undefined" &&
+  // Apparently the standard web API for detecting touch-primary devices.
+  window.matchMedia("(pointer: coarse)").matches;
+
 interface SlideTransitionProps {
   transitionKey: any;
   className?: string;
@@ -53,15 +60,21 @@ export default function QuoteCard({ quotes }: { quotes: NumberedQuote[] }) {
         <h4 className="w-fit text-sm text-gruvbox-gray">
           Quote #{curQuote.num} of {quotes.length}
         </h4>
-        <button
+        <motion.button
           type="button"
+          whileTap={{ scale: 0.94 }}
+          transition={{
+            type: "spring",
+            duration: isTouchDevice ? 0.125 : 0.075,
+            bounce: 0,
+          }}
           onClick={() => setCurQuoteIdx((prev) => (prev + 1) % quotes.length)}
-          className="transition:scale relative flex cursor-pointer items-center gap-2 rounded-lg bg-gruvbox-bg0-s px-4 py-2 text-sm shadow-md ring-1 ring-neutral-950/15 duration-75 before:pointer-events-none before:absolute before:inset-0 before:rounded-lg before:shadow-highlight before:shadow-white/5 hover:bg-gruvbox-bg1 active:scale-94"
+          className="relative flex cursor-pointer items-center gap-2 rounded-lg bg-gruvbox-bg0-s px-4 py-2 text-sm shadow-md ring-1 ring-neutral-950/15 select-none before:pointer-events-none before:absolute before:inset-0 before:rounded-lg before:shadow-highlight before:shadow-white/5 hover:bg-gruvbox-bg1"
         >
           <span className="sr-only">Reroll</span>
           <RerollIcon />
           Reroll
-        </button>
+        </motion.button>
       </header>
 
       <div className="flex flex-col gap-2">
