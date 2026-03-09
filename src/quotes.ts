c00b206f40239524e19ab3101dc0b5421ba07dc8
Nick Chaloult <npchaloult@gmail.com>
2026-03-09 16:35:45 -0400

    Add "they have no idea where their culture came from" quote

--- src/quotes.ts
+++ src/quotes.ts
@@ -268,4 +268,12 @@ export const quotes: Quote[] = [
       isAURL: true,
     },
   },
+  {
+    text: "Computing is pop culture. [...] Pop culture holds a disdain for history. Pop culture is all about identity and feeling like you're participtaing. It has nothing to do with cooperation, the past or the future - it's living in the present. I think the same is true of most peope who write code for money. They have no idea where [their culture came from].",
+    author: 'Alan Kay',
+    source: {
+      text: "in an interview with Dr. Dobb's Journal (2012)",
+      isAURL: false,
+    },
+  },
 ];
